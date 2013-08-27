0067d953cf4a27cf75ddcb16102f726c49ddd89a
Dave Bourke <squire40@gmail.com>
2013-08-26 20:41:52 -0400

    slarg

--- LambdaJamFSharpDemo/Program.fs
+++ LambdaJamFSharpDemo/Program.fs
@@ -3,17 +3,19 @@
 
 open FSharp.Data
 
+[<Literal>]
+let apiKey = "AIzaSyAUA6Pt5n4IyMlAc-5r2SM0SCuEUDHVsVg"
+type freebaseDataProvider = FreebaseDataProvider<Key=apiKey>
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-
-    let data = FreebaseData.GetDataContext()
-
-    
-
+    printfn "%A" argv    
+    let data = freebaseDataProvider.GetDataContext() 
+    data.DataContext.Limit <- 10
     let elements = data.``Arts and Entertainment``.``Fictional Universes``.``Fictional Characters`` |> Seq.toList
-    //.``Science and Technology``.Chemistry.``Chemical Elements`` |> Seq.toList
 
-    
+//    let rec getData = 
+        
+    printfn "%A" elements
     let hydrogen = data.``Science and Technology``.Chemistry.``Chemical Elements``.Individuals.Hydrogen 
     0 // return an integer exit code
