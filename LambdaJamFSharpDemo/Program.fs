6df2dbf017ab46f3b13123ee8b5d83760f792b04
Dave Bourke <squire40@gmail.com>
2013-09-02 19:18:56 -0400

    slarg

--- LambdaJamFSharpDemo/Program.fs
+++ LambdaJamFSharpDemo/Program.fs
@@ -11,18 +11,27 @@ type freebaseDataProvider = FreebaseDataProvider<Key=apiKey>
 let main argv = 
     printfn "%A" argv    
     let data = freebaseDataProvider.GetDataContext() 
-//    data.DataContext.Limit <- 10
+    data.DataContext.Limit <- 100
     printfn "Getting heroes"
     let heroes = query { for h in data.``Arts and Entertainment``.``Fictional Universes``.``Fictional Characters`` do
-                            where (h.``Appears In These Fictional Universes``.Equals "Marvel") }
-//                            where (not (h.``Powers or Abilities``.Equals "")) }
+                            where ((h.``Appears In These Fictional Universes``.Equals "Marvel") && (not(h.``Powers or Abilities``.Equals "")))
+                            select (h.Name, h.Gender, h.``Powers or Abilities``) }
                             |> Seq.toList
-    let heroesWithPowers = query { for h in heroes do
-                                    where (not (h.``Powers or Abilities``.Equals"")) }
-                                    |> Seq.toList
 
-    let powers = query { for p in heroesWithPowers do
-                            select p.``Powers or Abilities`` }
-                            |> Seq.toList
+//    let heroList = heroes |> Seq.toList
+//    let heroesWithPowers = query { for h in heroes do
+//                                    where (not (h.``Powers or Abilities``.Equals"")) }
+//                                    |> Seq.toList
+
+//    let powers = query { for p in heroesWithPowers do
+//                            select p.``Powers or Abilities`` }
+//                            |> Seq.toList
+
+//    let doOutput = (fun s -> Seq.iter |> System.Console.WriteLine s)
+//    doOutput powers
+//    for p in powers do
+//        System.Console.WriteLine "%A" p
+//        printfn "%A" p
+
     let input = System.Console.ReadKey
     0 // return an integer exit code
