3ad5f134e85a0412aece278f5bdc3078ae1b74ea
Dave Bourke <squire40@gmail.com>
2013-09-02 14:11:57 -0400

    slarg

--- LambdaJamFSharpDemo/Program.fs
+++ LambdaJamFSharpDemo/Program.fs
@@ -15,26 +15,14 @@ let main argv =
     printfn "Getting heroes"
     let heroes = query { for h in data.``Arts and Entertainment``.``Fictional Universes``.``Fictional Characters`` do
                             where (h.``Appears In These Fictional Universes``.Equals "Marvel") }
+//                            where (not (h.``Powers or Abilities``.Equals "")) }
                             |> Seq.toList
+    let heroesWithPowers = query { for h in heroes do
+                                    where (not (h.``Powers or Abilities``.Equals"")) }
+                                    |> Seq.toList
 
-    let powers = query { for p in heroes do
+    let powers = query { for p in heroesWithPowers do
                             select p.``Powers or Abilities`` }
                             |> Seq.toList
-//    let heroes = data.``Arts and Entertainment``.``Fictional Universes``.``Fictional Characters``.Individuals // |> Seq.toList
-//    let blah = heroes
-//    let getHeroes = 
-//        query { for h in heroes do
-//                where (h. == "Marvel")
-//                select h 
-//                }
-//        |> Seq.toList
-//        
-    let presidents = 
-        query { for e in data.Society.Government.``US Presidents`` do 
-                select e.Name } 
-        |> Seq.toList
-
-    printfn "%A" presidents
-    let blah = presidents
     let input = System.Console.ReadKey
     0 // return an integer exit code
