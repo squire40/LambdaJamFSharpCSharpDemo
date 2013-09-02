1cd66954b4d64413baa56e07fdfaa6df93ac80d2
Dave Bourke <squire40@gmail.com>
2013-09-02 12:52:34 -0400

    slarg

--- LambdaJamFSharpDemo/Program.fs
+++ LambdaJamFSharpDemo/Program.fs
@@ -13,14 +13,22 @@ let main argv =
     let data = freebaseDataProvider.GetDataContext() 
 //    data.DataContext.Limit <- 10
     printfn "Getting heroes"
-    let heroes = Seq.toList data.``Arts and Entertainment``.``Fictional Universes``.``Fictional Characters``.Individuals
-    let getHeroes = 
-        query { for h in heroes do
-                where (h. == "Marvel")
-                select h 
-                }
-        |> Seq.toList
-        
+    let heroes = query { for h in data.``Arts and Entertainment``.``Fictional Universes``.``Fictional Characters`` do
+                            where (h.``Appears In These Fictional Universes``.Equals "Marvel") }
+                            |> Seq.toList
+
+    let powers = query { for p in heroes do
+                            select p.``Powers or Abilities`` }
+                            |> Seq.toList
+//    let heroes = data.``Arts and Entertainment``.``Fictional Universes``.``Fictional Characters``.Individuals // |> Seq.toList
+//    let blah = heroes
+//    let getHeroes = 
+//        query { for h in heroes do
+//                where (h. == "Marvel")
+//                select h 
+//                }
+//        |> Seq.toList
+//        
     let presidents = 
         query { for e in data.Society.Government.``US Presidents`` do 
                 select e.Name } 
