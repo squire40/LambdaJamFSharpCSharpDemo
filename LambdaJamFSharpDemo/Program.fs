f5102813d296bf877b4e34520767a8b6419595e7
Dave Bourke <squire40@gmail.com>
2013-10-07 18:02:05 -0400

    slarg

--- LambdaJamFSharpDemo/Program.fs
+++ LambdaJamFSharpDemo/Program.fs
@@ -4,41 +4,54 @@ open System.Linq
 
 [<Literal>]
 let apiKey = @"AIzaSyAUA6Pt5n4IyMlAc-5r2SM0SCuEUDHVsVg"
-type freebaseDataProvider = FreebaseDataProvider<Key=apiKey>
-type hero = {Name:string; Gender:string list; Powers:string list}
+
+type freebaseDataProvider = FreebaseDataProvider< Key=apiKey >
+
+type hero = 
+    { Name : string
+      Gender : string list
+      Powers : string list }
+
 type power = 
-    {
-        Power : string;
-        Gender : string;
-        Count : int } 
+    { Power : string
+      Gender : string
+      Count : int }
 
 [<EntryPoint>]
 let main argv = 
     let watch = System.Diagnostics.Stopwatch.StartNew()
-
     let data = freebaseDataProvider.GetDataContext()
-//    printf "Getting heroes"
+    let fictionalChars = 
+        data.``Arts and Entertainment``.``Fictional Universes``.``Fictional Universes``
     
-    let fictionalChars = data.``Arts and Entertainment``.``Fictional Universes``.``Fictional Universes``
-
-    let marvel = query { 
-                        for h in fictionalChars do
-                        where (h.Name.Equals "Marvel Universe")
-                        }
-                        |> Seq.exactlyOne
-
-    let heroes = marvel.Characters
-                    |> Seq.filter(fun x -> x.``Powers or Abilities``.Any())
-                    |> Seq.toList
-
-    let heroesWithPowers =
-        heroes
-        |> List.map (fun y -> { Name   = y.Name; 
-                                Gender = y.Gender |> Seq.map string |> Seq.toList
-                                Powers = y.``Powers or Abilities``|> Seq.map string |> Seq.toList })
+    let marvel = 
+        query { 
+            for h in fictionalChars do
+                where (h.Name.Equals "Marvel Universe")
+        }
+        |> Seq.exactlyOne
     
-    let maleHeroes   = heroesWithPowers |> List.filter (fun i -> i.Gender.Contains("Male"))
-    let femaleHeroes = heroesWithPowers |> List.filter (fun i -> i.Gender.Contains("Female"))
+    let heroes = 
+        marvel.Characters
+        |> Seq.filter (fun x -> x.``Powers or Abilities``.Any())
+        |> Seq.toList
+    
+    let heroesWithPowers = 
+        heroes |> List.map (fun y -> 
+                      { Name = y.Name
+                        Gender = 
+                            y.Gender
+                            |> Seq.map string
+                            |> Seq.toList
+                        Powers = 
+                            y.``Powers or Abilities``
+                            |> Seq.map string
+                            |> Seq.toList })
+    
+    let maleHeroes = 
+        heroesWithPowers |> List.filter (fun i -> i.Gender.Contains("Male"))
+    let femaleHeroes = 
+        heroesWithPowers |> List.filter (fun i -> i.Gender.Contains("Female"))
     let powersFilter x y = y.Powers |> List.exists (fun z -> z = x)
     
     let powers = 
@@ -48,27 +61,35 @@ let main argv =
         |> Seq.sortBy (fun z -> z.Name)
         |> Seq.map (fun x' -> x'.Name)
         |> Seq.toList
-
+    
     let orderedCounter a b filter = 
         a
-        |> List.map (fun x -> x, b |> List.filter (filter x) |> List.length)
+        |> List.map (fun x -> 
+               x, 
+               b
+               |> List.filter (filter x)
+               |> List.length)
         |> List.sortBy (fun x -> snd x)
         |> List.rev
-
-    let topTenPowers heroes' =
+    
+    let topTenPowers heroes' = 
         (powers, heroes', powersFilter)
         |||> orderedCounter
         |> Seq.take 10
         |> Seq.toList
-
+    
     let topTenPowersByCountForMen = maleHeroes |> topTenPowers
     let topTenPowersByCountForWomen = femaleHeroes |> topTenPowers
-
     watch.Stop()
-    System.Console.WriteLine(System.String.Format("Time to get and slice data: {0} minutes, {1} seconds, {2} milliseconds", watch.Elapsed.Minutes, watch.Elapsed.Seconds, watch.Elapsed.Milliseconds));
+    System.Console.WriteLine
+        (System.String.Format
+             ("Time to get and slice data: {0} minutes, {1} seconds, {2} milliseconds", 
+              watch.Elapsed.Minutes, watch.Elapsed.Seconds, 
+              watch.Elapsed.Milliseconds))
     System.Console.ReadKey() |> ignore
     System.Console.Clear()
-    System.Console.WriteLine(System.String.Format("Total Super Heroes: {0}", heroes.Count()))
+    System.Console.WriteLine
+        (System.String.Format("Total Super Heroes: {0}", heroes.Count()))
     System.Console.WriteLine()
     System.Console.WriteLine("Top 10 Super Powers by Count")
     System.Console.WriteLine()
@@ -80,39 +101,33 @@ let main argv =
     System.Console.ReadKey() |> ignore
     for p in topTenPowersByCountForMen do
         printfn "Count: %i \tPower: %s" (snd p) (fst p)
-
     System.Console.ReadKey() |> ignore
     System.Console.Clear()
     printfn "Top Ten Super Powers for women\r\n"
     System.Console.ReadKey() |> ignore
     for p in topTenPowersByCountForWomen do
         printfn "Count: %i \tPower: %s" (snd p) (fst p)
-
     System.Console.ReadKey() |> ignore
-
-    let womenHavingTopTenPowers = 
-        
-    printfn "Slarg"
-    printfn "Slarg"
+    System.Console.Clear()
+    printfn "Female heroes having the top 5 powers:\r\n\r\n"
+    for p in topTenPowersByCountForWomen do
+        let power' = snd p
+        printfn ""
     //(snd(p), fst (p))
-//    for h in heroes do
-//        printfn "%A" h
-//    let heroList = heroes |> Seq.toList
-//    let heroesWithPowers = query { for h in heroes do
-//                                    where (not (h.``Powers or Abilities``.Equals"")) }
-//                                    |> Seq.toList
-
-
-    
-//    let powers = query { for p in heroes do
-//                            select p.``Powers or Abilities`` }
-////                            |> Seq.toList
-
-//    let doOutput = (fun s -> Seq.iter |> System.Console.WriteLine s)
-//    doOutput powers
-//    for p in powers do
-//        System.Console.WriteLine "%A" p
-//        printfn "%A" p
-
+    //    for h in heroes do
+    //        printfn "%A" h
+    //    let heroList = heroes |> Seq.toList
+    //    let heroesWithPowers = query { for h in heroes do
+    //                                    where (not (h.``Powers or Abilities``.Equals"")) }
+    //                                    |> Seq.toList
+    //    let powers = query { for p in heroes do
+    //                            select p.``Powers or Abilities`` }
+    ////                            |> Seq.toList
+    //    let doOutput = (fun s -> Seq.iter |> System.Console.WriteLine s)
+    //    doOutput powers
+    //    for p in powers do
+    //        System.Console.WriteLine "%A" p
+    //        printfn "%A" p
     System.Console.ReadKey() |> ignore
     0 // return an integer exit code
+      
