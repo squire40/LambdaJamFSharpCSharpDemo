0c0849843715ba7be1c516b3d8f3166b84906e37
Dave Bourke <squire40@gmail.com>
2013-10-14 22:17:55 -0400

    slarg

--- LambdaJamFSharpDemo/Program.fs
+++ LambdaJamFSharpDemo/Program.fs
@@ -35,9 +35,9 @@ let main argv =
         marvel.Characters
         |> Seq.filter (fun x -> x.``Powers or Abilities``.Any())
         |> Seq.toList
-
+    
     let heroesWithPowers = 
-        heroes  |> List.map (fun y -> 
+        heroes |> List.map (fun y -> 
                       { Name = y.Name
                         Gender = 
                             y.Gender
@@ -47,7 +47,7 @@ let main argv =
                             y.``Powers or Abilities``
                             |> Seq.map string
                             |> Seq.toList })
-
+    
     let maleHeroes = 
         heroesWithPowers |> List.filter (fun i -> i.Gender.Contains("Male"))
     let femaleHeroes = 
@@ -58,7 +58,7 @@ let main argv =
         heroes
         |> Seq.collect (fun y -> y.``Powers or Abilities``)
         |> Seq.distinctBy (fun x -> x.Name)
-//        |> Seq.sortBy (fun z -> z.Name)
+        //        |> Seq.sortBy (fun z -> z.Name)
         |> Seq.map (fun x' -> x'.Name)
         |> Seq.toList
     
@@ -81,13 +81,12 @@ let main argv =
     let topTenPowersByCountForMen = maleHeroes |> topTenPowers
     let topTenPowersByCountForWomen = femaleHeroes |> topTenPowers
     watch.Stop()
-
     //TODO: Refactor for in |> List.iter
     let doPowersOutput hlist = 
-        (topTenPowers hlist) |> List.iter (fun p -> printfn "Count: %i \tPower: %s" (snd p) (fst p))
+        (topTenPowers hlist) 
+        |> List.iter (fun p -> printfn "Count: %i \tPower: %s" (snd p) (fst p))
         System.Console.ReadKey() |> ignore
         System.Console.Clear()
-
     System.Console.WriteLine
         (System.String.Format
              ("Time to get and slice data: {0} minutes, {1} seconds, {2} milliseconds", 
@@ -97,42 +96,38 @@ let main argv =
     System.Console.Clear()
     printfn "Total Super Heroes: %i\r\n" heroes.Length
     printfn "Top 10 Super Powers by Count\r\n"
-
     doPowersOutput heroesWithPowers
     printfn "Top Ten Super Powers for men\r\n"
     doPowersOutput maleHeroes
     printfn "Top Ten Super Powers for women\r\n"
     doPowersOutput femaleHeroes
-    
     let printHerosHavingPowerList (powersList : (string * int) list) 
         (heroesList : hero list) = 
-        powersList |> Seq.take 5 
-        |> Seq.iter 
-            (fun p -> 
-                let power' = fst p 
-                let hlist = 
-                    heroesList
-                    |> List.filter (fun x -> x.Powers.Contains power')
-                    |> List.sortBy (fun y -> y.Name.ToLower())
-                printfn "Power: %s\r\n" power'
-                for i in 0..hlist.Length - 1 do
-                    if i > 0 && i % 15 = 0 then 
-                        printfn "\r\nPress any key...\r\n"
-                        System.Console.ReadKey() |> ignore
-                        System.Console.Clear()
-                        printfn "Power: %s\r\n" power'
-                    printfn "%s" (hlist.Item(i).Name)
-                System.Console.ReadKey() |> ignore
-                System.Console.Clear()
-
-                System.Console.ReadKey |> ignore
-                System.Console.Clear |> ignore)
-
+        powersList
+        |> Seq.take 5
+        |> Seq.iter (fun p -> 
+               let power' = fst p
+               
+               let hlist = 
+                   heroesList
+                   |> List.filter (fun x -> x.Powers.Contains power')
+                   |> List.sortBy (fun y -> y.Name.ToLower())
+               printfn "Power: %s\r\n" power'
+               for i in 0..hlist.Length - 1 do
+                   if i > 0 && i % 15 = 0 then 
+                       printfn "\r\nPress any key...\r\n"
+                       System.Console.ReadKey() |> ignore
+                       System.Console.Clear()
+                       printfn "Power: %s\r\n" power'
+                   printfn "%s" (hlist.Item(i).Name)
+               System.Console.ReadKey() |> ignore
+               System.Console.Clear()
+               System.Console.ReadKey |> ignore
+               System.Console.Clear |> ignore)
     printfn "Female heroes having the top 5 powers:\r\n"
     printHerosHavingPowerList topTenPowersByCountForWomen femaleHeroes
     printfn "Male heroes having the top 5 powers:\r\n"
     printHerosHavingPowerList topTenPowersByCountForMen maleHeroes
-    
     System.Console.ReadKey() |> ignore
     0 // return an integer exit code
       
